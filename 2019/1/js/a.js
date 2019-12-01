590d34c9c098bea0a862d910e4d963a059df8e9f
qwelyt <qwelyt@users.noreply.github.com>
2019-12-01 22:57:00 +0100

    Move JS to subfolder

--- 2019/1/js/a.js
+++ 2019/1/js/a.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname, 'input.txt');
+const filePath = path.join(__dirname, '../input.txt');
 
 const file = fs.readFileSync(filePath, {encoding: 'UTF-8'}, function(err, data){
   if(err){
