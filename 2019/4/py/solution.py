b881c9475dca837cb0919ce778168613c2282e0f
qwelyt <qwelyt@users.noreply.github.com>
2019-12-04 22:32:52 +0100

    Add py solution for Day 4.2

--- 2019/4/py/solution.py
+++ 2019/4/py/solution.py
@@ -11,6 +11,16 @@ def verifyDoubleSequence(n):
     last = c
   return False
 
+def verifyExclusiveDoubleSequence(n):
+  chars = list(str(n))
+  sdict = {}
+  for c in chars:
+    if(c not in sdict):
+      sdict[c] = 0
+    sdict[c] += 1
+  return 2 in sdict.values()
+
+
 def verifyIncreasingSequence(n):
   nums = list(map(lambda n: int(n),list(str(n))))
   last = None
@@ -22,33 +32,41 @@ def verifyIncreasingSequence(n):
 
   return True
 
-def verify(number):
+def verify(number, verifySequence):
   if(not verifyLength(number)):
      return False
-  if(not verifyDoubleSequence(number)):
+  if(not verifySequence(number)):
      return False
   if(not verifyIncreasingSequence(number)):
      return False
   return True
 
-def generate(start, end):
+def generate(start, end, verifySequence):
   ans = []
   for number in range(start,end+1):
-    if(verify(number)):
+    if(verify(number, verifySequence)):
       ans.append(number)
   return ans
 
 def solve(input):
   range = list(map(lambda s: int(s), input.split("-")))
-  nums = generate(range[0], range[1])
+  nums = generate(range[0], range[1], verifyDoubleSequence)
   return nums
+
+def solveB(input):
+  range = list(map(lambda s: int(s), input.split("-")))
+  nums = generate(range[0], range[1], verifyExclusiveDoubleSequence)
+  return nums
+
     
 
 
 
 input = "284639-748759"
-result = solve(input)
-print(result)
+
+resultA = solve(input)
+resultB = solveB(input)
 print("----")
-print(len(result))
+print("A", len(resultA), len(resultA) == 895)
+print("B", len(resultB), len(resultB) == 591)
 
