b46f69569cc12d6a255f8c28ab425ebdb19f0ae7
qwelyt <qwelyt@users.noreply.github.com>
2019-12-12 00:49:33 +0100

    Try to get 10.2 to work

--- 2019/10/js/solution.js
+++ 2019/10/js/solution.js
@@ -1,7 +1,9 @@
 const point = (_x,_y) => {return {x:_x, y:_y}}
 const degrees = r => r/Math.PI
-const angle = (a,b) => Math.atan2(b.x-a.x, -(b.y-a.y) ) % (2 * Math.PI)
+const angle = (a,b) => Math.atan2(a.x-b.x, a.y-b.y) % (2 * Math.PI)
+const distance = (a,b) => Math.sqrt((a.x - b.x)**2 + (a.y-b.y)**2)
 const str = a => a.x+','+a.y
+
 
 function asteroids(arr){
   let roids = []
@@ -59,28 +61,81 @@ lenghts
   return max
 }
 
-  const map = ".#..##.###...#######" +
-  "\n##.############..##." +
-  "\n.#.######.########.#" +
-  "\n.###.#######.####.#." +
-  "\n#####.##.#.##.###.##" +
-  "\n..#####..#.#########" +
-  "\n####################" +
-  "\n#.####....###.#.#.##" +
-  "\n##.#################" +
-  "\n#####.##.###..####.." +
-  "\n..######..##.#######" +
-  "\n####.##.####...##..#" +
-  "\n.#####..#.######.###" +
-  "\n##...#.##########..." +
-  "\n#.##########.#######" +
-  "\n.####.#.###.###.#.##" +
-  "\n....##.##.###..#####" +
-  "\n.#.#.###########.###" +
-  "\n#.#.#.#####.####.###" +
-  "\n###.##.####.##.#..##" 
-            solveA(map)
+const arrToObj = a => {
+    const o = {}
+    o[a[0]] = a[1]
+    return o
+}
+
+function solveB(map, base){
+  const arr = map.split('\n').map(s => s.split(''))
+  const roids = asteroids(arr).filter(a => !(a.y == base.y && a.x==base.x))
+
+  let ang = {}
+  for(asteroid of roids){
+    let a=  360 - degrees(angle(base, asteroid))
+    if(a == 360) a = 0
+
+    if(!ang.hasOwnProperty(a)) ang[a] = []
+    ang[a].push(asteroid)
+  }
+  const sortByAngle = Object.entries(ang).map(a => [Number.parseFloat(a[0]), a[1]]).sort((a,b) => a[0] >= b[0])
+  const obj = sortByAngle.reduce((a,c) => {
+    const k = c[0]
+    a[k] = c[1]
+    return a
+  }, {})
+  let sortByDistance = sortByAngle.map(a => [a[0], a[1].sort((a,b) => {
+                                    const aDist = distance(a,base)
+                                    const bDist = distance(b, base)
+                                    return aDist >= bDist
+                                  })
+                       ])
+
+
+  let destroyed = 0
+  for(let i=0; i<sortByDistance.length; ++i){
+    let roid = sortByDistance[i]
+    if(roid){
+      const r = roid[1].pop()
+      ++destroyed
+      if(destroyed == 200){
+        return r
+      }
+    }
+    if(i == sortByDistance.lenght-1) i = 0
+  }
+  return "lolwut"
+}
+
+  const map = ".###.#...#.#.##.#.####.."+
+  "\n.#....#####...#.######.." +
+  "\n#.#.###.###.#.....#.####" +
+  "\n##.###..##..####.#.####." +
+  "\n###########.#######.##.#" +
+  "\n##########.#########.##." +
+  "\n.#.##.########.##...###." +
+  "\n###.#.##.#####.#.###.###" +
+  "\n##.#####.##..###.#.##.#." +
+  "\n.#.#.#####.####.#..#####" +
+  "\n.###.#####.#..#..##.#.##" +
+  "\n########.##.#...########" +
+  "\n.####..##..#.###.###.#.#" +
+  "\n....######.##.#.######.#" +
+  "\n###.####.######.#....###" +
+  "\n############.#.#.##.####" +
+  "\n##...##..####.####.#..##" +
+  "\n.###.#########.###..#.##" +
+  "\n#.##.#.#...##...#####..#" +
+  "\n##.#..###############.##" +
+  "\n##.###.#####.##.######.." +
+  "\n##.#####.#.#.##..#######" +
+  "\n...#######.######...####" +
+  "\n#....#.#.#.####.#.#.#.##" 
+
+            solveB(map, {x:20,y:18})
 
 module.exports = {
   solveA: solveA
+  , solveB: solveB
 }
