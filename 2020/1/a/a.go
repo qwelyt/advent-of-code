1ed6098bc82202653e8fa3477f3df65e741093e5
qwelyt <qwelyt@users.noreply.github.com>
2020-12-01 20:12:32 +0000

    Add testing for a

--- 2020/1/a/a.go
+++ 2020/1/a/a.go
@@ -1,18 +1,18 @@
-package main;
+package main
 
 import (
-	"fmt"
 	"bufio"
-	"os"
+	"fmt"
 	"log"
+	"os"
 	"strconv"
-);
+)
 
 type Pair struct {
 	left, right int
 }
 
-func readFile(filePath string) []int{
+func ReadFile(filePath string) []int {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -26,7 +26,7 @@ func readFile(filePath string) []int{
 
 	var values []int
 	for scanner.Scan() {
-		value,err := strconv.Atoi(scanner.Text())
+		value, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -35,24 +35,28 @@ func readFile(filePath string) []int{
 	return values
 }
 
-func findPairThatSumsToValue(values []int, target int) Pair{
-	for i :=0; i < len(values); i++ {
+func SumToTarget(values []int, target int) Pair {
+	for i := 0; i < len(values); i++ {
 		for j := 0; j < len(values); j++ {
-			if(i == j) {
+			if i == j {
 				continue
 			}
-			if(values[i] + values[j] == target) {
+			if values[i]+values[j] == target {
 				return Pair{values[i], values[j]}
 			}
 		}
 	}
-	return Pair{0,0}
+	return Pair{0, 0}
 }
 
-func main(){
-	values := readFile("../input.txt")
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+func main() {
+	values := ReadFile("../input.txt")
 	//values := readFile("./example.txt")
-	pair := findPairThatSumsToValue(values, 2020)
-	multiply := pair.left * pair.right
+	pair := SumToTarget(values, 2020)
+	multiply := Multiply(pair.left, pair.right)
 	fmt.Println(multiply)
 }
