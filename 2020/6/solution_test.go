91868317970ab45a51e6e384bfa3fba5fdca5aec
qwelyt <qwelyt@users.noreply.github.com>
2020-12-06 12:49:15 +0000

    Add solutions to day 6

---
+++ 2020/6/solution_test.go
@@ -0,0 +1,44 @@
+package main
+
+import "testing"
+
+func TestCountGroupAnyYes(t *testing.T) {
+	verifyAnyGroupCount(t, []string{"abc"}, 3)
+	verifyAnyGroupCount(t, []string{"a", "b", "c"}, 3)
+}
+
+func TestCountGroupAllYes(t *testing.T) {
+	verifyAllGroupCount(t, []string{"abc"}, 3)
+	verifyAllGroupCount(t, []string{"a", "b", "c"}, 0)
+}
+
+func verifyAnyGroupCount(t *testing.T, input []string, expected int) {
+	result := CountGroupAnyYes(input)
+	if expected != result {
+		t.Errorf("Expected: %d, Got: %d", expected, result)
+	}
+}
+
+func verifyAllGroupCount(t *testing.T, input []string, expected int) {
+	result := CountGroupAllYes(input)
+	if expected != result {
+		t.Errorf("Expected: %d, Got: %d", expected, result)
+	}
+}
+
+func TestCountAnyYes(t *testing.T) {
+	input := ReadFile("example.txt")
+	expected := 11
+	result := CountAnyYes(input)
+	if expected != result {
+		t.Errorf("Expected: %d, Got: %d", expected, result)
+	}
+}
+func TestCountAllYes(t *testing.T) {
+	input := ReadFile("example.txt")
+	expected := 6
+	result := CountAllYes(input)
+	if expected != result {
+		t.Errorf("Expected: %d, Got: %d", expected, result)
+	}
+}
