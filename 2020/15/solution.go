f64fcb31039db94958c3e4c8be77852afbcdffab
qwelyt <qwelyt@users.noreply.github.com>
2020-12-15 15:55:14 +0000

    15: Add test, look at doing things differently

--- 2020/15/solution.go
+++ 2020/15/solution.go
@@ -14,29 +14,37 @@ func PartA(input []int, spokenAt int) int {
 	// 3: If the previous spoken number has been spoken before, take the previous turn number minus the time before that that that number
 	//		was spoken. Speak the difference.
 
+	// Consider using a queue of size 2 instead of an ever growing arr
+	// From https://stackoverflow.com/questions/2818852/is-there-a-queue-implementation
+	//Or however many you might need + buffer.
+	// c := make(chan int, 300)
+	//Push
+	// c <- value
+	//Pop
+	// x <- c
+
 	var spoken = make(map[int][]int)
-	var lastSpoken []int
+	var lastSpoken int
 	// Rule 1
 	for i := 0; i < len(input); i++ {
 		spoken[input[i]] = []int{i + 1}
-		lastSpoken = append(lastSpoken, input[i])
+		lastSpoken = input[i]
 	}
 
 	for i := len(input) + 1; i < spokenAt+1; i++ {
-		ls := lastSpoken[len(lastSpoken)-1] // Get the last spoken number
-		th := spoken[ls]                    // Get all times that the last spoken number has been spoken
-		if len(th) <= 1 {                   // Rule 2: The number has never been spoken OR the last time was the first time it was spoken
-			lastSpoken = append(lastSpoken, 0)
+		th := spoken[lastSpoken] // Get all times that the last spoken number has been spoken
+		if len(th) <= 1 {        // Rule 2: The number has never been spoken OR the last time was the first time it was spoken
+			lastSpoken = 0
 			spoken[0] = append(spoken[0], i) // Since we spoke 0, append current turn to when 0 has been spoken
 		} else { // Rule 3: The last spoken number has been spoken before
 			previousTimes := th[len(th)-2:]
 			diff := previousTimes[1] - previousTimes[0]
-			lastSpoken = append(lastSpoken, diff)
+			lastSpoken = diff
 			spoken[diff] = append(spoken[diff], i)
 		}
 	}
 
-	return lastSpoken[len(lastSpoken)-1]
+	return lastSpoken
 }
 
 func main() {
