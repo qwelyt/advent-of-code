d54ddea9278ad4b9444f8eb26eb5a264d22c8729
qwelyt <qwelyt@users.noreply.github.com>
2020-12-15 23:06:39 +0100

    15 Add alternative that uses channels
    Add an alternative solution that uses channels instead of arrays. Not
    sure if this is faster or not, but I wanted to try it out. I didn't find
    a way to just peek into the channel to get values. You have to pop them
    and then push them again if you want them to survive.
    
    The best would have been if the first message on the channel would "fall
    off" when we push a new value. And that values would stay when we peeked
    them.

--- 2020/15/solution.go
+++ 2020/15/solution.go
@@ -14,15 +14,6 @@ func PartA(input []int, spokenAt int) int {
 	// 3: If the previous spoken number has been spoken before, take the previous turn number minus the time before that that that number
 	//		was spoken. Speak the difference.
 
-	// Consider using a queue of size 2 instead of an ever growing arr
-	// From https://stackoverflow.com/questions/2818852/is-there-a-queue-implementation
-	//Or however many you might need + buffer.
-	// c := make(chan int, 300)
-	//Push
-	// c <- value
-	//Pop
-	// x <- c
-
 	var spoken = make(map[int][]int)
 	var lastSpoken int
 	// Rule 1
@@ -50,6 +41,53 @@ func PartA(input []int, spokenAt int) int {
 	return lastSpoken
 }
 
+func PartAChannels(input []int, spokenAt int) int {
+	// RULES
+	// 1: Speak all starting (input) numbers, they don't look at the previous spoken numbers.
+	// 2: If the previous spoken number was the first time that number was spoken, say 0
+	// 3: If the previous spoken number has been spoken before, take the previous turn number minus the time before that that that number
+	//		was spoken. Speak the difference.
+
+	var spoken = make(map[int](chan int))
+	var lastSpoken int
+	// Rule 1
+	for i := 0; i < len(input); i++ {
+		spoken[input[i]] = make(chan int, 2)
+		spoken[input[i]] <- i + 1
+		lastSpoken = input[i]
+	}
+
+	for i := len(input) + 1; i < spokenAt+1; i++ {
+		th := spoken[lastSpoken] // Get all times that the last spoken number has been spoken
+		if len(th) <= 1 {        // Rule 2: The number has never been spoken OR the last time was the first time it was spoken
+			lastSpoken = 0
+			if len(spoken[0]) == 2 {
+				<-spoken[0]
+			}
+			spoken[0] <- i // Since we spoke 0, append current turn to when 0 has been spoken
+		} else { // Rule 3: The last spoken number has been spoken before
+			first := <-th
+			second := <-th
+			diff := second - first
+			if spoken[diff] == nil {
+				spoken[diff] = make(chan int, 2)
+			}
+			if diff != lastSpoken {
+				if len(spoken[diff]) == 2 {
+					<-spoken[diff]
+				}
+				spoken[diff] <- i
+				spoken[lastSpoken] <- first // Put 'em back
+				spoken[lastSpoken] <- second
+			} else {
+				spoken[diff] <- second
+				spoken[diff] <- i
+			}
+			lastSpoken = diff
+		}
+	}
+	return lastSpoken
+}
 func main() {
 	input := []int{16, 1, 0, 18, 12, 14, 19}
 	fmt.Printf("=== Part A ===\n2020s number: %d\n", PartA(input, 2020))
