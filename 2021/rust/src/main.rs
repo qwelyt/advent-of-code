cfedcb6b06ff49a894d60a5b74bf514bb53e202b
qwelyt <qwelyt@users.noreply.github.com>
2021-12-15 22:58:59 +0100

    Solve day 15

--- 2021/rust/src/main.rs
+++ 2021/rust/src/main.rs
@@ -13,6 +13,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 
 
 fn main() {
@@ -30,4 +31,5 @@ fn main() {
     day12::day12();
     day13::day13();
     day14::day14();
+    day15::day15();
 }
