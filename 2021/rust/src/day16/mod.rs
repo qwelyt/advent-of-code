c8235ff5647ea0ffb8b3de913fc3069900341cfa
qwelyt <qwelyt@users.noreply.github.com>
2021-12-18 20:35:24 +0100

    FINALLY solve 16.2

--- 2021/rust/src/day16/mod.rs
+++ 2021/rust/src/day16/mod.rs
@@ -1,8 +1,4 @@
-use std::usize;
-
 use crate::util::lines_from_file;
-
-mod other;
 
 pub fn day16() {
     println!("== Day 16 ==");
@@ -13,144 +9,29 @@ pub fn day16() {
     println!("Part B: {}", b);
 }
 
-fn part_a(input: &Vec<String>) -> usize {
+fn part_a(input: &Vec<String>) -> u32 {
     let packet = input.get(0).unwrap();
     // println!("{}", packet);
     let binary = to_binary(packet);
     // println!("{:?}", binary);
-    let (versions, _data, _value) = parse(&binary, None);
-    versions.iter().sum()
+    let (poop, _iter) = solve(&binary, 0);
+    // println!("{:?}", poop);
+    poop.versions.iter().sum()
 }
 
-fn part_b(input: &Vec<String>) -> u128 {
+fn part_b(input: &Vec<String>) -> u64 {
     let packet = input.get(0).unwrap();
     // println!("{}", packet);
     let binary = to_binary(packet);
+    println!("{} bits", binary.len());
     // println!("{:?}", binary);
-    let (_versions, _data, value) = parse(&binary, None);
-
-    return *value.get(0).unwrap();
+    let (poop, _iter) = solve(&binary, 0);
+    println!("{:?}", poop);
+    return *poop.numbers.get(0).unwrap()
 }
 
 
-fn parse(binary: &Vec<char>, operations: Option<usize>) -> (Vec<usize>, Vec<char>, Vec<u128>) {
-    // println!("Received: {:?} ::({}) {:?}", operations, binary.len(), binary);
-    if binary.iter().map(|c| c.to_digit(10).unwrap()).sum::<u32>() == 0 {
-        return (Vec::new(), Vec::new(), Vec::new());
-    }
 
-    let mut versions: Vec<usize> = Vec::new();
-
-    let mut data = binary.clone();
-
-
-    let mut ops = 0;
-    let mut values: Vec<u128> = Vec::new();
-    let f: fn(Vec<u128>, usize) -> u128 = |v: Vec<u128>, type_id: usize| {
-        match type_id {
-            0 => v.into_iter().fold(0, |acc, add| acc + add),
-            1 => v.into_iter().fold(1, |acc, add| acc * add),
-            2 => v.into_iter().fold(u128::MAX, |a, b| std::cmp::min(a, b)),
-            3 => v.into_iter().fold(0, |a, b| std::cmp::max(a, b)),
-            5 => (v[0] > v[1]) as u128,
-            6 => (v[0] < v[1]) as u128,
-            7 => (v[0] == v[1]) as u128,
-            _ => { 0 }
-        }
-    };
-    while data.len() > 0 && ops < operations.unwrap_or(usize::MAX) {
-        if data.iter().map(|c| c.to_digit(10).unwrap()).sum::<u32>() == 0 {
-            break;
-        }
-        // println!("## Data is: {:?}", data);
-        let version = usize::from_str_radix(data[0..3].to_vec().iter().collect::<String>().as_str(), 2).unwrap();
-        let type_id = usize::from_str_radix(data[3..6].to_vec().iter().collect::<String>().as_str(), 2).unwrap();
-        data = data.split_at(6).1.to_vec();
-        // println!("## Version: {:?}", version);
-        // println!("## type: {:?}", type_id);
-
-        versions.push(version);
-        match type_id {
-            4 => {
-                // Literal value
-                ops += 1;
-                let mut numbers: Vec<Vec<char>> = Vec::new();
-                let mut left: Vec<Vec<char>> = Vec::new();
-                let mut done = false;
-                for c in data.as_slice().chunks(5) {
-                    if c[0] == '0' {
-                        if c.len() == 5 && !done {
-                            done = true;
-                            numbers.push(c[1..5].to_vec());
-                            continue;
-                        } else {
-                            left.push(c.to_vec())
-                        }
-                    } else {
-                        if !done {
-                            numbers.push(c[1..5].to_vec());
-                        } else {
-                            left.push(c.to_vec());
-                        }
-                    }
-                }
-                // println!("====== Numbers: {:?}", numbers);
-                // for n in numbers.iter() {
-                //     let number = usize::from_str_radix(n.iter().collect::<String>().as_str(), 2).unwrap();
-                //     println!("{:?} = {}", n, number);
-                // }
-                // println!("======  Left: {:?}", left);
-                let big_num: Vec<char> = numbers.into_iter().flatten().collect::<Vec<char>>();
-                let number = usize::from_str_radix(big_num.iter().collect::<String>().as_str(), 2).unwrap();
-                values.push(number as u128);
-                data = left.iter().flatten().map(|c| *c).collect();
-            }
-            _ => {
-                println!("## type: {:?}", type_id);
-                // Operator
-                let length = if data[0] == '0' { 15 } else { 11 };
-                data = data.split_at(1).1.to_vec();
-                // println!("Operator, length {}", length);
-
-                let number_bits = data[0..length].to_vec();
-                let number = usize::from_str_radix(number_bits.to_vec().iter().collect::<String>().as_str(), 2).unwrap();
-
-                // println!("Number is {}", number);
-
-                data = data.split_at(length).1.to_vec();
-
-                if length == 15 {
-                    // sub_package says the next 'number' of bits is the sub_pkg
-                    let sub_packages = data[0..number as usize].to_vec();
-                    let (mut sub_versions, _left, sub_values) = parse(&sub_packages, None);
-                    versions.append(&mut sub_versions);
-                    // println!("15 left :: {} {:?}", _left.len(), _left);
-                    let v = f(sub_values, type_id);
-                    println!("o: {} ::: v: {}", type_id, v);
-                    values.push(v);
-
-                    data = data.split_at(number as usize).1.to_vec();
-                } else if length == 11 {
-                    // number of sub packages contained by this package
-                    // println!("11 ||| n: {}  d: {:?}", number, data);
-                    let (mut sub_versions, left, sub_values) = parse(&data, Some(number));
-                    versions.append(&mut sub_versions);
-                    let v = f(sub_values, type_id);
-                    println!("o: {} ::: v: {}", type_id, v);
-                    values.push(v);
-                    data = left.clone();
-                }
-            }
-        }
-    }
-
-    // println!("Versions: {:?}", versions);
-    // println!("Data: {:?}", data);
-    // println!("Ops: {}", ops);
-    // println!("Value: {:?}", values);
-
-    (versions, data.to_vec(), values)
-}
 //     // keep mut vec with all the data left in binary
 //     // When we find an operator, take out the sub-vec needed
 //     // for that operation and call this method with the sub
@@ -163,6 +44,112 @@ fn parse(binary: &Vec<char>, operations: Option<usize>) -> (Vec<usize>, Vec<char
 //     // Also need to return the bits not processed yet.
 //     // So a (Vec<usize>, Vec<char>) return? Will probably have to expand for p2.
 //     // And a (binary: &Vec<char>, operations: Option<usize>) header?
+#[derive(Debug)]
+struct Poop {
+    bits_drained: usize,
+    numbers: Vec<u64>,
+    versions: Vec<u32>,
+}
+
+fn solve(binary: &Vec<char>, iteration: u32) -> (Poop, u32) {
+    let mut bits = binary.clone();
+    let version = u32::from_str_radix(bits.drain(0..3).collect::<String>().as_str(), 2).unwrap();
+    let type_id = u32::from_str_radix(bits.drain(0..3).collect::<String>().as_str(), 2).unwrap();
+    // println!("Iteration: {} :: Version: {} :: Type_id: {} ::: Bin/bit {}/{}", iteration, version, type_id, binary.len() ,bits.len());
+    let mut bits_drained = 6; // Version and type_id
+    let mut versions = vec![version];
+
+    return match type_id {
+        4 => {
+            let mut numbers: Vec<Vec<char>> = Vec::new();
+            // println!("Bits: ({}) {:?}", bits.len(), bits);
+            loop {
+                let mut n = bits.drain(0..5).collect::<Vec<char>>();
+                let head = n.drain(0..1).collect::<Vec<char>>()[0];
+                numbers.push(n.clone());
+                if head == '0' { break; }
+            }
+            bits_drained += numbers.len();
+            let big_num: Vec<char> = numbers.into_iter().flatten().collect::<Vec<char>>();
+            bits_drained += big_num.len();
+            let number = u64::from_str_radix(big_num.iter().collect::<String>().as_str(), 2).unwrap();
+            // println!("I found {} after draining {} from ({}){:?}", number, bits_drained,binary.len(), binary);
+            // println!("bd: i {} {}", iteration, bits_drained);
+            (Poop {
+                bits_drained,
+                numbers: vec![number],
+                versions,
+            }, iteration)
+        }
+        _ => {
+            let length = if *bits.get(0).unwrap() == '0' { 15 } else { 11 };
+            bits.drain(0..1);
+            bits_drained += 1;
+
+            // println!("Binary: {} {:?}",binary.len(), binary);
+            // println!("Bits: {} {:?}", bits.len(), bits);
+            let number = u32::from_str_radix(bits.drain(0..length).collect::<String>().as_str(), 2).unwrap();
+            // println!("Number: {}", number);
+            // println!("Length: {}", length);
+            bits_drained += length;
+
+            let mut numbers = Vec::new();
+            if length == 15 {
+                // If the length type ID is 0, then the next 15 bits are a
+                // number that represents the total length in bits of the
+                // sub-packets contained by this packet.
+                let mut sub_pkg = bits.drain(0..number as usize).collect::<Vec<char>>();
+                bits_drained += number as usize;
+                // println!("L15 Solve for: ({}) {:?}",sub_pkg.len(), sub_pkg);
+                let mut drained = 0;
+                while drained < number as usize {
+                    let (poop, _iter) = solve(&sub_pkg, iteration + 1);
+                    // println!("L15 poop (i {}): {:?}", _iter, poop);
+                    drained += poop.bits_drained;
+                    sub_pkg.drain(0..poop.bits_drained);
+                    numbers.append(&mut poop.numbers.clone());
+                    versions.append(&mut poop.versions.clone());
+                }
+            } else {
+                // Length 11:
+                // If the length type ID is 1, then the next 11 bits are a
+                // number that represents the number of sub-packets immediately
+                // contained by this packet.
+                let mut sub_pkg_done = 0;
+                while sub_pkg_done < number {
+                    // println!("L11 Solve for: ({}) {:?}", bits.len(), bits);
+                    let (poop, _iter) = solve(&bits.clone(), iteration + 1);
+                    // println!("L11 poo (i {})p: {:?} ::: ({}) {:?}", _iter, poop, bits.len(), bits);
+                    numbers.append(&mut poop.numbers.clone());
+                    versions.append(&mut poop.versions.clone());
+                    bits.drain(0..poop.bits_drained);
+                    bits_drained += poop.bits_drained;
+                    sub_pkg_done += 1;
+                }
+            }
+            // println!("bd: i {} {}", iteration, bits_drained);
+            (Poop {
+                bits_drained,
+                numbers: vec![calc(numbers, type_id)],
+                versions,
+            }, iteration)
+        }
+    };
+}
+
+
+fn calc(values: Vec<u64>, type_id: u32) -> u64 {
+    match type_id {
+        0 => values.into_iter().sum(), // 0 = Sum
+        1 => values.into_iter().product(), // 1 = Product
+        2 => values.into_iter().min().unwrap(), // 2 = min
+        3 => values.into_iter().max().unwrap(), // 3 = max
+        5 => { if values[0] > values[1] { 1 } else { 0 } }, // 5 = packet[0] > packet[1] (greater than)
+        6 => { if values[0] < values[1] { 1 } else { 0 } }, // 6 = packet[0] < packet[1] (less than)
+        7 => { if values[0] == values[1] { 1 } else { 0 } }, // 7 = packet[0] == packet[1] (equal)
+        _ => { panic!("Got {} as type_id!", type_id) }
+    }
+}
 
 fn to_binary(hex: &String) -> Vec<char> {
     hex.chars()
@@ -203,8 +190,8 @@ mod tests {
     #[test]
     fn parse_to_decimal_t() {
         let binary = "110100101111111000101000".chars().collect();
-        let (a, b, c) = parse(&binary, None);
-        assert_eq!(2021, *c.get(0).unwrap())
+        let (poop, _iter) = solve(&binary, 0);
+        assert_eq!(2021, *poop.numbers.get(0).unwrap())
     }
 
     #[test]
@@ -307,7 +294,7 @@ mod tests {
         let filename = "src/day16/test-input.txt";
         let input = lines_from_file(filename);
         let result = part_b(&input);
-        assert_eq!(31, result)
+        assert_eq!(54, result)
     }
 
     #[test]
@@ -315,6 +302,33 @@ mod tests {
         let filename = "src/day16/input.txt";
         let input = lines_from_file(filename);
         let result = part_b(&input);
-        assert_ne!(10760589785870465, result)
+        assert_ne!(10760589785870465, result);
+        assert_eq!(true, result < 10760589785870465);
+        assert_ne!(680615, result);
+        assert_eq!(true, result > 680615);
+        assert_ne!(319751467161448, result);
+        assert_eq!(true, 319751467161448 > result);
+        assert_eq!(194435634456, result);
+    }
+
+
+    #[test]
+    fn solve_t_1() {
+        let input = "C200B40A82".to_string();
+        let binary = to_binary(&input);
+        // println!("{:?}", binary);
+        let (result, _iter) = solve(&binary, 0);
+
+        assert_eq!(3, *result.numbers.get(0).unwrap())
+    }
+
+    #[test]
+    fn solve_t_2() {
+        let input = "EE00D40C823060".to_string();
+        let binary = to_binary(&input);
+        // println!("{:?}", binary);
+        let (result, _iter) = solve(&binary, 0);
+
+        assert_eq!(3, *result.numbers.get(0).unwrap())
     }
 }
