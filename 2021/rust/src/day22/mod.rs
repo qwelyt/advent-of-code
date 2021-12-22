81a67810c1f28bc14835ba8240893da710af8cba
qwelyt <qwelyt@users.noreply.github.com>
2021-12-22 22:38:16 +0100

    Add fast way for 22.2 and solve it

--- 2021/rust/src/day22/mod.rs
+++ 2021/rust/src/day22/mod.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::str::FromStr;
-use std::thread::current;
 
+use crate::day22::Direction::{Back, Bottom, Front, Left, Right, Top};
 use crate::util::lines_from_file;
 
 pub fn day22() {
@@ -20,11 +20,14 @@ fn part_a(input: &Vec<String>) -> usize {
     // for i in instructions.iter() {
     //     println!("{:?}", i);
     // }
-    follow_instructions(&instructions)
+    follow_instructions_a(&instructions)
 }
 
-fn part_b(input: &Vec<String>) -> u32 {
-    todo!()
+fn part_b(input: &Vec<String>) -> usize {
+    let instructions: Vec<Instruction> = input.iter()
+        .map(|s| to_instruction(s))
+        .collect();
+    follow_instructions_b(&instructions)
 }
 
 fn to_instruction(line: &String) -> Instruction {
@@ -47,7 +50,7 @@ fn to_instruction(line: &String) -> Instruction {
     Instruction { on, cuboid }
 }
 
-fn follow_instructions(instructions: &Vec<Instruction>) -> usize {
+fn follow_instructions_a(instructions: &Vec<Instruction>) -> usize {
     let mut set: HashSet<Cube> = HashSet::new();
     for instruction in instructions.iter() {
         let vec: Vec<Cube> = instruction.cuboid.cubes_in_range(-50, 50);
@@ -65,6 +68,68 @@ fn follow_instructions(instructions: &Vec<Instruction>) -> usize {
     set.len()
 }
 
+fn follow_instructions_b(instructions: &Vec<Instruction>) -> usize {
+    let mut cuboids: Vec<Cuboid> = Vec::new();
+
+    for instruction in instructions.iter() {
+        let current_cuboid = instruction.cuboid;
+        let mut new_cuboids = Vec::new();
+
+        if instruction.on {
+            new_cuboids.push(current_cuboid);
+        }
+
+        for c in cuboids.iter() {
+            let mut cuboid = c.clone();
+            if !cuboid.overlaps(current_cuboid) {
+                new_cuboids.push(cuboid);
+            } else {
+                // Slice'n'dice with NicerDicer+
+                if cuboid.x.0 < current_cuboid.x.0 {
+                    new_cuboids.push(cuboid.slice(current_cuboid, Right));
+                    cuboid.x.0 = current_cuboid.x.0;
+                }
+
+                if cuboid.x.1 > current_cuboid.x.1 {
+                    new_cuboids.push(cuboid.slice(current_cuboid, Left));
+                    cuboid.x.1 = current_cuboid.x.1;
+                }
+
+                if cuboid.y.0 < current_cuboid.y.0 {
+                    new_cuboids.push(cuboid.slice(current_cuboid, Top));
+                    cuboid.y.0 = current_cuboid.y.0;
+                }
+
+                if cuboid.y.1 > current_cuboid.y.1 {
+                    new_cuboids.push(cuboid.slice(current_cuboid, Bottom));
+                    cuboid.y.1 = current_cuboid.y.1;
+                }
+
+                if cuboid.z.0 < current_cuboid.z.0 {
+                    new_cuboids.push(cuboid.slice(current_cuboid, Front));
+                    cuboid.z.0 = current_cuboid.z.0;
+                }
+
+                if cuboid.z.1 > current_cuboid.z.1 {
+                    new_cuboids.push(cuboid.slice(current_cuboid, Back));
+                    cuboid.z.1 = current_cuboid.z.1;
+                }
+            }
+        }
+        cuboids = new_cuboids.clone();
+    }
+
+
+    cuboids.iter()
+        .map(|c| {
+            let x = (c.x.0 - c.x.1 - 1).abs() as usize;
+            let y = (c.y.0 - c.y.1 - 1).abs() as usize;
+            let z = (c.z.0 - c.z.1 - 1).abs() as usize;
+            return x * y * z;
+        })
+        .sum()
+}
+
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 struct Cube {
     x: i64,
@@ -80,19 +145,20 @@ struct Cuboid {
     z: (i64, i64),
 }
 
+enum Direction {
+    Left,
+    Right,
+    Top,
+    Bottom,
+    Front,
+    Back,
+}
+
 impl Cuboid {
     fn overlaps(self, other: Cuboid) -> bool {
-        let self_x: HashSet<i64> = HashSet::from_iter((self.x.0..=self.x.1));
-        let self_y: HashSet<i64> = HashSet::from_iter((self.y.0..=self.y.1));
-        let self_z: HashSet<i64> = HashSet::from_iter((self.z.0..=self.z.1));
-        let other_x = HashSet::from_iter((other.x.0..=other.x.1));
-        let other_y = HashSet::from_iter((other.y.0..=other.y.1));
-        let other_z = HashSet::from_iter((other.z.0..=other.z.1));
-
-        let x = self_x.intersection(&other_x).map(|x| *x).collect::<Vec<i64>>();
-        let y = self_y.intersection(&other_y).map(|x| *x).collect::<Vec<i64>>();
-        let z = self_z.intersection(&other_z).map(|x| *x).collect::<Vec<i64>>();
-        !x.is_empty() && !y.is_empty() && !z.is_empty()
+        !(i64::max(self.x.0, other.x.0) > i64::min(self.x.1, other.x.1)
+            || i64::max(self.y.0, other.y.0) > i64::min(self.y.1, other.y.1)
+            || i64::max(self.z.0, other.z.0) > i64::min(self.z.1, other.z.1))
     }
 
     fn cubes_in_range(self, a: i64, b: i64) -> Vec<Cube> {
@@ -108,6 +174,53 @@ impl Cuboid {
             }
         }
         vec
+    }
+
+    fn slice(self, other: Cuboid, cut: Direction) -> Cuboid {
+        match cut {
+            Direction::Right => {
+                Cuboid {
+                    x: (self.x.0, other.x.0 - 1),
+                    y: self.y,
+                    z: self.z,
+                }
+            }
+            Direction::Left => {
+                Cuboid {
+                    x: (other.x.1 + 1, self.x.1),
+                    y: self.y,
+                    z: self.z,
+                }
+            }
+            Direction::Top => {
+                Cuboid {
+                    x: self.x,
+                    y: (self.y.0, other.y.0 - 1),
+                    z: self.z,
+                }
+            }
+            Direction::Bottom => {
+                Cuboid {
+                    x: self.x,
+                    y: (other.y.1 + 1, self.y.1),
+                    z: self.z,
+                }
+            }
+            Direction::Front => {
+                Cuboid {
+                    x: self.x,
+                    y: self.y,
+                    z: (self.z.0, other.z.0 - 1),
+                }
+            }
+            Direction::Back => {
+                Cuboid {
+                    x: self.x,
+                    y: self.y,
+                    z: (other.z.1 + 1, self.z.1),
+                }
+            }
+        }
     }
 }
 
@@ -135,7 +248,7 @@ mod tests {
         let filename = "src/day22/input.txt";
         let input = lines_from_file(filename);
         let result = part_a(&input);
-        assert_eq!(576600, result);
+        assert_eq!(602574, result);
     }
 
     #[test]
@@ -151,6 +264,6 @@ mod tests {
         let filename = "src/day22/input.txt";
         let input = lines_from_file(filename);
         let result = part_b(&input);
-        assert_eq!(2, result);
+        assert_eq!(1288707160324706, result);
     }
 }
