73b5130ca1040b3e397b827b5544e9ee62dc7228
qwelyt <qwelyt@users.noreply.github.com>
2022-12-06 09:08:10 +0100

    Solve day6

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -4,6 +4,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     day1::day1();
@@ -11,4 +12,5 @@ fn main() {
     day3::day3();
     day4::day4();
     day5::day5();
+    day6::day6();
 }
