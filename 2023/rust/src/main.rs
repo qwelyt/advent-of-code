9912193d657e6078afaf4c2da7964e072d249f68
qwelyt <qwelyt@users.noreply.github.com>
2023-12-13 14:29:40 +0100

    Solve day 13
    Part 1 was fairly simple. Edge cases that are annoying.
    Part 2 was harder for some reason. I wanted to make a nicer solution
    but my matching method just didn't want to work. Reverted back to a
    brute force solution. Not pretty.

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -15,6 +15,7 @@ mod day9;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 
 fn main() {
     time_all(|| {
@@ -30,5 +31,6 @@ fn main() {
         day10::day10();
         day11::day11();
         day12::day12();
+        day13::day13();
     });
 }
