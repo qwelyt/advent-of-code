3aa783351ef841bad2bb498492bb0839faf1a27f
qwelyt <qwelyt@users.noreply.github.com>
2023-12-19 22:55:11 +0100

    Solve day 19
    Wooo! Not a grid problem! Biggest problem for p1 was parsing. Started
    off with using closures and other fancy things. Worked nicely. Then p2
    came around and that part had to change so I could actually match on the
     operand. Still works nicely as is. One _could_ quadrupled for-loops.
     And wait 1000 years for it to finish. Oooor you can start calculating
     on ranges.

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -21,6 +21,7 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
+mod day19;
 
 fn main() {
     time_all(|| {
@@ -42,5 +43,6 @@ fn main() {
         day16::day16();
         day17::day17();
         day18::day18();
+        day19::day19();
     });
 }
