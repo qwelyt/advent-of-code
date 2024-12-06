1fee199172a7ecf30bf8042599f1945bfc5b902b
Filip Bellander <filip.bellander@klarna.com>
2024-12-06 13:19:35 +0100

    Solve day 6

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -5,6 +5,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main(){
     util::time_all(|| {
@@ -13,5 +14,6 @@ fn main(){
         day3::solve();
         day4::solve();
         day5::solve();
+        day6::solve();
     })
 }
