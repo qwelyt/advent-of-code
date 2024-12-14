d889d87963fed2ebd3e3c6bc4348a451e8ff2869
Filip Bellander <filip.bellander@klarna.com>
2024-12-14 23:56:55 +0100

    Solve day 14
    Part 2 is not a good problem.

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -13,6 +13,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 
 fn main(){
     util::time_all(|| {
@@ -29,5 +30,6 @@ fn main(){
         day11::solve();
         day12::solve();
         day13::solve();
+        day14::solve();
     })
 }
