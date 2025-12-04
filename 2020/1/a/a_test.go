84c5dae84918299d5a58800600744e91bd81b4fa
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:30 +0100

    Add testing for a

---
+++ 2020/1/a/a_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReadFile(t *testing.T) {
+	expected := []int{1721, 979, 366, 299, 675, 1456}
+	result := ReadFile("../example.txt")
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("Value incorrect. Wanted %d, got %d", expected, result)
+	}
+}
+
+func TestSumToTarget(t *testing.T) {
+	values := []int{1721, 979, 366, 299, 675, 1456}
+	result := SumToTarget(values, 2020)
+	expected := Pair{1721, 299}
+	if result.left != expected.left {
+		t.Errorf("Value incorrect. Wanted %d, got %d", expected.left, result.left)
+	}
+	if result.right != expected.right {
+		t.Errorf("Value incorrect. Wanted %d, got %d", expected.right, result.right)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	result := Multiply(1721, 299)
+	expected := 514579
+	if result != expected {
+		t.Errorf("Value incorrect. Wanted %d, got %d", expected, result)
+	}
+}
