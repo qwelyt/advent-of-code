4445d4a2ebc4ac667ff2c9d83fa6f7961f9ccde5
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:35 +0100

    Solve day 7

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -6,6 +6,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 fn main(){
     util::time_all(|| {
@@ -15,5 +16,6 @@ fn main(){
         day4::solve();
         day5::solve();
         day6::solve();
+        day7::solve();
     })
 }
