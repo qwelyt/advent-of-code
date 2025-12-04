f0c748c713c5ec1edab2004f1b04d1f31d729488
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:34 +0100

    21.2 solved!
    Hhahaha, fun. Solved the problem in a test. Now I need to actually write
     a function for it.

--- 2023/rust/src/day21/mod.rs
+++ 2023/rust/src/day21/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -19,10 +19,13 @@ fn part_b(input: &str) -> usize {
     solve_b(input, 26501365)
 }
 
-fn solve_a(input: &str, steps: u8) -> usize {
+fn solve_a(input: &str, steps: u32) -> usize {
     let garden = Garden::parse(input);
     // println!("{:?}", garden);
     garden.possible_tiles(steps)
+        .values()
+        .filter(|distance| **distance % 2 == 0)
+        .count()
 }
 
 fn solve_b(input: &str, steps: u32) -> usize {
@@ -85,28 +88,34 @@ impl Garden {
         };
     }
 
-    fn possible_tiles(&self, steps: u8) -> usize {
-        let mut reachable = HashSet::new();
+    fn possible_tiles(&self, steps: u32) -> HashMap<Pos, u32> {
+        let mut reachable = HashMap::new();
 
         let delta = [(-1, 0), (0, -1), (1, 0), (0, 1)];
-        let mut queue = VecDeque::<Pos>::new();
-        queue.push_back(self.start);
-        reachable.insert(self.start);
+        let mut queue = VecDeque::<(Pos, u32)>::new();
+        queue.push_back((self.start, 0));
+        // reachable.insert(self.start, 0);
 
         // self._print(&reachable);
 
         let mut new_starts = HashSet::new();
-        for _i in 1..=steps {
+        for _i in 0..=steps {
             new_starts.into_iter().for_each(|p| queue.push_back(p));
             new_starts = HashSet::new();
-            while let Some(start) = queue.pop_front() {
-                reachable.insert(start);
+            if queue.len() == 0 {
+                break;
+            }
+            while let Some((start, distance)) = queue.pop_front() {
+                if reachable.contains_key(&start) {
+                    continue;
+                }
+                reachable.insert(start, distance);
                 for d in delta.iter() {
                     let new_pos = add(start, *d);
                     // print!("Going from {:?} to {:?}", start, new_pos);
                     if self.walkable(&new_pos) {
                         // println!(" -> Works! {:?}", self.map[new_pos.0 as usize][new_pos.1 as usize]);
-                        new_starts.insert(new_pos);
+                        new_starts.insert((new_pos, distance + 1));
                     } else {
                         // println!(" -> NO! walkable {:?}, reached: {:?}", self.walkable(&new_pos), reachable.contains(&new_pos));
                     }
@@ -119,21 +128,37 @@ impl Garden {
             // self._print(&reachable);
             // println!();
         }
-        // self._print(&reachable);
+        // let keys = reachable.keys().map(|k| *k).collect::<HashSet<Pos>>();
+        // self._print(&keys);
         // println!("===");
         // println!("===");
-        // self._print(&new_starts);
-        new_starts.len()
+        // self._print(&new_starts.iter().map(|t| t.0).collect());
+        // for e in reachable.iter() {
+        //     println!("{:?}", e);
+        // }
+        // new_starts.len()
+        reachable
     }
     fn possible_tiles_big(&self, steps: u32) -> usize {
         let y_tiles_to_edge = (self.size.0 - self.start.0) as u32;
         let x_tiles_to_edge = (self.size.1 - self.start.1) as u32;
         assert_eq!(y_tiles_to_edge, x_tiles_to_edge);
         if steps <= y_tiles_to_edge {
-            return self.possible_tiles(steps as u8);
+            return self.possible_tiles(steps)
+                .values()
+                .filter(|distance| **distance % 2 == 0)
+                .count();
         }
-        println!("{:?}", y_tiles_to_edge);
-        println!("{:?}", steps % y_tiles_to_edge);
+        let full_walk = self.possible_tiles(y_tiles_to_edge * 2);
+        let even = full_walk.values()
+            .filter(|distance| **distance % 2 == 0)
+            .count();
+        let odd = full_walk.values()
+            .filter(|distance| **distance % 2 == 1)
+            .count();
+        println!("len:  {:?}", full_walk.len());
+        println!("even: {:?}", even);
+        println!("odd:  {:?}", odd);
         // self.possible_tiles()
         0
     }
@@ -184,6 +209,104 @@ mod tests {
         let input = "src/day21/test-input.txt";
         let result = solve_a(input, 6);
         assert_eq!(16, result);
+    }
+
+    #[test]
+    fn garden_test() {
+        let input = "src/day21/input.txt";
+        let garden = Garden::parse(input);
+        let take_steps = 26501365;
+        println!("size {:?}", garden.size);
+        println!("start {:?}", garden.start);
+        assert_eq!(garden.size.0, garden.size.1);
+        println!("Total: {:?}", garden.size.0 as usize * garden.size.1 as usize);
+        let max_possible_steps_until_edge = garden.size.0 - garden.start.0;
+        println!("{:?}", max_possible_steps_until_edge);
+        println!("Num gardens in straight line {:?}", (take_steps - garden.start.1) / garden.size.1);
+        println!("Even garden? {:?} -- {:?}", garden.size.0 % 2 == 0, (garden.size.0 * garden.size.1) % 2 == 0);
+        let full_walk = garden.possible_tiles(take_steps as u32);
+        // Basically proves that 21.1 is walking to the edge
+        // So walking outside the edge would be like walking inside, kinda
+        let edge_walk = full_walk.values()
+            .filter(|v| **v < max_possible_steps_until_edge as u32)
+            .filter(|v| **v % 2 == 0)
+            .count();
+        assert_eq!(3729, edge_walk);
+
+        let corners = full_walk.values()
+            .filter(|v| **v >= max_possible_steps_until_edge as u32)
+            .collect::<Vec<&u32>>();
+        let corners_odd = full_walk.values()
+            .filter(|v| **v >= max_possible_steps_until_edge as u32)
+            .filter(|v| **v % 2 == 1)
+            .count();
+        let corners_even = full_walk.values()
+            .filter(|v| **v >= max_possible_steps_until_edge as u32)
+            .filter(|v| **v % 2 == 0)
+            .count();
+
+        println!("Corners: {:?}, odd {:?}, even {:?}", corners.len(), corners_odd, corners_even);
+        println!("{:?}", corners);
+
+        let total = full_walk.values().collect::<Vec<&u32>>();
+        let even = full_walk.values()
+            .filter(|v| **v % 2 == 0)
+            .count();
+        let odd = full_walk.values()
+            .filter(|v| **v % 2 == 1)
+            .count();
+        println!("Total: {:?}, odd {:?}, even {:?}", total.len(), odd, even);
+        println!("{:?}", total);
+
+        /*
+                 /\
+                /  \
+               /    \
+               \    /
+                \  /
+                 \/    Full gardens will be cut off....
+         */
+        let total_gardens = ((take_steps - garden.start.1) / garden.size.1) as usize;
+        let all_odds = (total_gardens + 1) * total_gardens * odd;
+        let all_evens = total_gardens * total_gardens * even;
+        let all_corners = (total_gardens + 1) * corners_odd + total_gardens * corners_even;
+
+        let ans = all_odds + all_evens - all_corners;
+        println!("{:?} WRONG", ans);
+
+
+        // let all_corners = ((total_gardens+1)*corners_odd) - total_gardens*corners_even;
+        let all_odd_corners = (total_gardens + 1) * corners_odd;
+        let all_even_corners = total_gardens * corners_even;
+        let ans = all_odds + all_evens - all_odd_corners + all_even_corners;
+        println!("{:?} WRONG TOO LOW", ans);
+
+        let total_gardens = ((take_steps - garden.start.1) / garden.size.1) as usize;
+        let extra_garden = total_gardens + 1;
+        let odd_garden = extra_garden * extra_garden;
+        let even_garden = total_gardens * total_gardens;
+        let all_odds = odd * odd_garden;
+        let all_evens = even * even_garden;
+        let odd_corners = extra_garden * corners_odd;
+        let even_corners = total_gardens * corners_even;
+        let ans = all_odds + all_evens + even_corners - odd_corners;
+        println!("{:?}", ans);
+        assert_eq!(621289922886149, ans);
+
+        // for steps in 0..garden.size.0 as u8 +10 {
+        //     let walk = garden.possible_tiles(steps);
+        //     let even = walk.values()
+        //         .filter(|distance| **distance % 2 == 0)
+        //         .count();
+        //     let odd = walk.values()
+        //         .filter(|distance| **distance % 2 == 1)
+        //         .count();
+        //     println!("Steps: {:?}: odd {:?}, even {:?}", steps, odd, even);
+        //     // println!("len:  {:?}", walk.len());
+        //     // println!("even: {:?}", even);
+        //     // println!("odd:  {:?}", odd);
+        //     // println!("=====================================================");
+        // }
     }
 
     #[test]
