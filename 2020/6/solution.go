02a33051235c08f62f8afb326d2492eba81e6f9f
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:30 +0100

    Add solutions to day 6

---
+++ 2020/6/solution.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func ReadFile(filePath string) []string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var values []string
+	for scanner.Scan() {
+		values = append(values, scanner.Text())
+	}
+	return values
+}
+
+func CountGroupAnyYes(group []string) int {
+	var m = make(map[string]bool)
+	for _, v := range group {
+		for i := 0; i < len(v); i++ {
+			c := string(v[i])
+			_, present := m[c]
+			if !present {
+				m[c] = true
+			}
+		}
+	}
+	return len(m)
+}
+
+func CountGroupAllYes(group []string) int {
+	var m = make(map[string]int)
+	var num int
+	for _, v := range group {
+		for i := 0; i < len(v); i++ {
+			c := string(v[i])
+			m[c] = m[c] + 1
+		}
+	}
+	for _, v := range m {
+		if v == len(group) {
+			num++
+		}
+	}
+	return num
+}
+
+func CountYesPerGroup(input []string, countFn func([]string) int) []int {
+	var yes []int
+	var group []string
+
+	for _, row := range input {
+		if len(row) == 0 {
+			y := countFn(group)
+			yes = append(yes, y)
+			group = nil
+		} else {
+			group = append(group, row)
+		}
+	}
+	if len(group) > 0 {
+		y := countFn(group)
+		yes = append(yes, y)
+		group = nil
+	}
+
+	return yes
+}
+
+func CountAnyYes(input []string) int {
+	var result int
+	ypg := CountYesPerGroup(input, CountGroupAnyYes)
+	for _, y := range ypg {
+		result = result + y
+	}
+	return result
+}
+
+func CountAllYes(input []string) int {
+	var result int
+	ypg := CountYesPerGroup(input, CountGroupAllYes)
+	for _, y := range ypg {
+		result = result + y
+	}
+	return result
+}
+
+func main() {
+	// input := ReadFile("example.txt")
+	input := ReadFile("input.txt")
+	partA := CountAnyYes(input)
+	fmt.Printf("=== Part A ===\nSum yeses: %d\n", partA)
+	partB := CountAllYes(input)
+	fmt.Printf("=== Part B ===\nSum yeses: %d\n", partB)
+}
