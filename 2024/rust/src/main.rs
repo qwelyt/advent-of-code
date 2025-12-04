747439861972bb66857b25e930ab32cfb4e7a9ab
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:35 +0100

    Day 3
    I wonder if this is possible without regex... and make it nicer without
    for-loops.

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -2,10 +2,12 @@ extern crate core;
 mod day1;
 mod util;
 mod day2;
+mod day3;
 
 fn main(){
     util::time_all(|| {
         day1::solve();
         day2::solve();
+        day3::solve();
     })
 }
