3b8b978c476f26d6eca29b7f2497f34d364e583e
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:31 +0100

    Solve day 10
    I have no idea how I solved 10.2. I just did TDD and bruteforced code
    that seemed to have worked... I need to figure out a solution that I
    actually understand because the one that I solved is not something I
    know how it works. It just works(TM). Somehow.

---
+++ 2020/10/solution_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestJoltageJump(t *testing.T) {
+	input := ReadFile("example.txt")
+	one, three := JoltageJumps(input)
+
+	if 7 != one {
+		t.Errorf("Expected %d one jumps, got %d", 7, one)
+	}
+	if 5 != three {
+		t.Errorf("Expected %d three jumps, got %d", 5, three)
+	}
+}
+func TestJoltageJump3(t *testing.T) {
+	input := ReadFile("sortedInput.txt")
+	one, three := JoltageJumps(input)
+
+	if 66 != one {
+		t.Errorf("Expected %d one jumps, got %d", 7, one)
+	}
+	if 30 != three {
+		t.Errorf("Expected %d three jumps, got %d", 5, three)
+	}
+}
+
+func TestJoltageJump2(t *testing.T) {
+	input := ReadFile("example2.txt")
+	one, three := JoltageJumps(input)
+
+	if 22 != one {
+		t.Errorf("Expected %d one jumps, got %d", 22, one)
+	}
+	if 10 != three {
+		t.Errorf("Expected %d three jumps, got %d", 10, three)
+	}
+}
+
+func TestDistinctArrangements(t *testing.T) {
+	input := ReadFile("example2.txt")
+	expected := 19208
+	result := DA(input)
+
+	if expected != result {
+		t.Errorf("Expected: %d, Got: %d", expected, result)
+	}
+}
