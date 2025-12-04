b4e665329e9085ce954b5822bbc87b9178d1a15b
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:35 +0100

    Day 20.1 solved
    20.2 is ... oh boy can this blow up.

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -19,6 +19,7 @@ mod day16;
 mod day17;
 mod day18;
 mod day19;
+mod day20;
 
 fn main(){
     util::time_all(|| {
@@ -41,5 +42,6 @@ fn main(){
         day17::solve();
         day18::solve();
         day19::solve();
+        day20::solve();
     })
 }
