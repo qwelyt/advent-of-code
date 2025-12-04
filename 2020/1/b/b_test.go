8d35cdc5b7264f414cf12fc9891807e617d023df
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:30 +0100

    Add tests for 2020-1b

---
+++ 2020/1/b/b_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReadFile(t *testing.T) {
+	expected := []int{1721, 979, 366, 299, 675, 1456}
+	result := ReadFile("../example.txt")
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("Value incorrect. Wanted %d, got %d", expected, result)
+	}
+}
+
+func TestSumToTarget(t *testing.T) {
+	values := []int{1721, 979, 366, 299, 675, 1456}
+	result := SumToTarget(values, 2020)
+	expected := Tuple3{979, 366, 675}
+	if result != expected {
+		t.Errorf("Value incorrect. Wanted %d, got %d", expected, result)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	tuple := Tuple3{979, 366, 675}
+	result := Multiply(tuple)
+	expected := 241861950
+	if result != expected {
+		t.Errorf("Value incorrect. Wanted %d, got %d", expected, result)
+	}
+}
