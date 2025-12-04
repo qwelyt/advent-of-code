898ca7a5daa387c7266d6fe6b79a8ac6c8655637
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:31 +0100

    Solve 15
    There probably is a faster way to do this as 15.2 took around 6 seconds
    to run. But doing it faster will probably make it more complicated.
    Aint nobody got time for that.

---
+++ 2020/15/solution_test.go
@@ -0,0 +1,54 @@
+package main
+
+import "testing"
+
+func TestPartA(t *testing.T) {
+	input := []int{0, 3, 6}
+	expected := 436
+	result := PartA(input, 2020)
+
+	if expected != result {
+		t.Errorf("Expected: %d, Got: %d", expected, result)
+	}
+}
+
+func TestPartAExamples(t *testing.T) {
+	var cases = map[int][]int{
+		436:  {0, 3, 6},
+		1:    {1, 3, 2},
+		10:   {2, 1, 3},
+		27:   {1, 2, 3},
+		78:   {2, 3, 1},
+		438:  {3, 2, 1},
+		1836: {3, 1, 2},
+	}
+
+	for expected, input := range cases {
+		result := PartA(input, 2020)
+
+		if expected != result {
+			t.Errorf("Expected: %d, Got: %d", expected, result)
+		}
+	}
+}
+
+func TestPartBExamples(t *testing.T) {
+	var cases = map[int][]int{
+
+		175594:  {0, 3, 6},
+		2578:    {1, 3, 2},
+		3544142: {2, 1, 3},
+		261214:  {1, 2, 3},
+		6895259: {2, 3, 1},
+		18:      {3, 2, 1},
+		362:     {3, 1, 2},
+	}
+
+	for expected, input := range cases {
+		result := PartA(input, 30000000)
+
+		if expected != result {
+			t.Errorf("Expected: %d, Got: %d", expected, result)
+		}
+	}
+}
