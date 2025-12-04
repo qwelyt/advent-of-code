7a243b13d37f296b0b2cbb88259868af4f264062
qwelyt <qwelyt@users.noreply.github.com>
2025-12-04 08:36:29 +0100

    Refactor

--- 2019/2/js/a.js
+++ 2019/2/js/a.js
@@ -1,26 +1,22 @@
-const operand = (arr,i,offset) => arr[arr[i+offset]];
+const location = (arr,i,offset) => arr[i+offset];
+const operand = (arr,i,offset) => arr[location(arr,i,offset)];
+const addition = (a,b) => a+b;
+const multiplication = (a,b) => a*b;
+const apply = (f,a,b) => f(a,b);
 
-function process(inputArr){
-  let arr = [].concat(inputArr);
+const operate = (arr, i) => {
+  const op = arr[i];
+  if(op == 99) return;
+  if(op == 1) return apply(addition, operand(arr,i,1), operand(arr,i,2));
+  if(op == 2) return apply(multiplication, operand(arr,i,1), operand(arr,i,2));
+  throw new Error("Unknown op-code: "+op);
+}
+
+function process(input){
+  let arr = [].concat(input);
   for(var i=0,len=arr.length; i<len; i=i+4){
-    let operation = arr[i];
-    if(operation == 99) {
-      break;
-    } else if (operation == 1){
-      // addition
-      let operand1 = operand(arr,i,1);
-      let operand2 = operand(arr,i,2);
-      let storeLocation = arr[i+3];
-      arr[storeLocation] = operand1 + operand2;
-    } else if(operation == 2){
-      // multiplication
-      let operand1 = operand(arr,i,1);
-      let operand2 = operand(arr,i,2);
-      let storeLocation = arr[i+3];
-      arr[storeLocation] = operand1 * operand2;
-    } else {
-      throw new Error("Unknown op-code: "+i);
-    }
+    if(arr[i] == 99){break;}
+    arr[location(arr,i,3)] = operate(arr, i);
   }
   return arr;
 }
